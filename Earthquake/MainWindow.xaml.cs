63a3d6826a4a830392e2e7b0adc50cc62a682f5b
Trevor Takawira <trevortaks@gmail.com>
2019-02-12 21:49:51 +0200

    Broke functionality down into functions

--- Earthquake/MainWindow.xaml.cs
+++ Earthquake/MainWindow.xaml.cs
@@ -35,49 +35,73 @@ namespace QuakeData
         public MainWindow()
         {
             InitializeComponent();
-            List<Ellipse> points = new List<Ellipse>();
+            initialiseData(baseurl);
+        }
+
+        public void initialiseData(string url) {
+            Dictionary<System.Windows.Point, Ellipse> points = new Dictionary<System.Windows.Point, Ellipse>();
 
             try
-             {
-                 using (WebClient wc = new WebClient())
-                 {
-                     var json = wc.DownloadString(baseurl);
+            {
+                root = getData(url);
+            }
+            catch (System.Net.WebException e)
+            {
+                lblStatus.Content = e.Message;
+                string json = "{\"type\":\"FeatureCollection\",\"metadata\":{\"generated\":1549544968000,\"url\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-02-07&minmagnitude=5\",\"title\":\"USGS Earthquakes\",\"status\":200,\"api\":\"1.7.0\",\"count\":3},\"features\":[{\"type\":\"Feature\",\"properties\":{ \"mag\":5.4,\"place\":\"99km ENE of Tobelo, Indonesia\",\"time\":1549533788290,\"updated\":1549534764040,\"tz\":540,\"url\":\"https://earthquake.usgs.gov/earthquakes/eventpage/us2000jepg\",\"detail\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us2000jepg&format=geojson\",\"felt\":null,\"cdi\":null,\"mmi\":null,\"alert\":null,\"status\":\"reviewed\",\"tsunami\":1,\"sig\":449,\"net\":\"us\",\"code\":\"2000jepg\",\"ids\":\",us2000jepg,\",\"sources\":\",us,\",\"types\":\",geoserve,origin,phase-data,\",\"nst\":null,\"dmin\":2.002,\"rms\":1.45,\"gap\":106,\"magType\":\"mb\",\"type\":\"earthquake\",\"title\":\"M 5.4 - 99km ENE of Tobelo, Indonesia\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[128.7647,2.2151,33]},\"id\":\"us2000jepg\"},{\"type\":\"Feature\",\"properties\":{\"mag\":5.7,\"place\":\"129km NW of Kota Ternate, Indonesia\",\"time\":1549512929410,\"updated\":1549520267062,\"tz\":480,\"url\":\"https://earthquake.usgs.gov/earthquakes/eventpage/us2000jekx\",\"detail\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us2000jekx&format=geojson\",\"felt\":2,\"cdi\":4.5,\"mmi\":4.37,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":0,\"sig\":501,\"net\":\"us\",\"code\":\"2000jekx\",\"ids\":\",us2000jekx,\",\"sources\":\",us,\",\"types\":\",dyfi,geoserve,losspager,origin,phase-data,shakemap,\",\"nst\":null,\"dmin\":1.168,\"rms\":1.1,\"gap\":91,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 5.7 - 129km NW of Kota Ternate, Indonesia\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[126.4563,1.5093,35]},\"id\":\"us2000jekx\"},{\"type\":\"Feature\",\"properties\":{\"mag\":5.5,\"place\":\"43km NE of Auki, Solomon Islands\",\"time\":1549503694180,\"updated\":1549511014814,\"tz\":660,\"url\":\"https://earthquake.usgs.gov/earthquakes/eventpage/us2000jek8\",\"detail\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us2000jek8&format=geojson\",\"felt\":3,\"cdi\":2.7,\"mmi\":4.01,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":0,\"sig\":466,\"net\":\"us\",\"code\":\"2000jek8\",\"ids\":\",us2000jek8,\",\"sources\":\",us,\",\"types\":\",dyfi,geoserve,losspager,origin,phase-data,shakemap,\",\"nst\":null,\"dmin\":1.391,\"rms\":0.85,\"gap\":50,\"magType\":\"mb\",\"type\":\"earthquake\",\"title\":\"M 5.5 - 43km NE of Auki, Solomon Islands\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[160.9592,-8.4642,65.94]},\"id\":\"us2000jek8\"}],\"bbox\":[126.4563,-8.4642,33,160.9592,2.2151,65.94]}";
+                root = JsonConvert.DeserializeObject<RootObject>(json);
+            }
+            finally
+            {
+                points = getQuake();
+                Draw(points);
+            }
+        }
 
-                     root = JsonConvert.DeserializeObject<RootObject>(json);
-                     //Console.WriteLine(root.metadata.url);
-                    
-                 }
-             }
-             catch (Exception e) {
-                 lblStatus.Content =  e;
-                 string json = "{\"type\":\"FeatureCollection\",\"metadata\":{\"generated\":1549544968000,\"url\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-02-07&minmagnitude=5\",\"title\":\"USGS Earthquakes\",\"status\":200,\"api\":\"1.7.0\",\"count\":3},\"features\":[{\"type\":\"Feature\",\"properties\":{ \"mag\":5.4,\"place\":\"99km ENE of Tobelo, Indonesia\",\"time\":1549533788290,\"updated\":1549534764040,\"tz\":540,\"url\":\"https://earthquake.usgs.gov/earthquakes/eventpage/us2000jepg\",\"detail\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us2000jepg&format=geojson\",\"felt\":null,\"cdi\":null,\"mmi\":null,\"alert\":null,\"status\":\"reviewed\",\"tsunami\":1,\"sig\":449,\"net\":\"us\",\"code\":\"2000jepg\",\"ids\":\",us2000jepg,\",\"sources\":\",us,\",\"types\":\",geoserve,origin,phase-data,\",\"nst\":null,\"dmin\":2.002,\"rms\":1.45,\"gap\":106,\"magType\":\"mb\",\"type\":\"earthquake\",\"title\":\"M 5.4 - 99km ENE of Tobelo, Indonesia\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[128.7647,2.2151,33]},\"id\":\"us2000jepg\"},{\"type\":\"Feature\",\"properties\":{\"mag\":5.7,\"place\":\"129km NW of Kota Ternate, Indonesia\",\"time\":1549512929410,\"updated\":1549520267062,\"tz\":480,\"url\":\"https://earthquake.usgs.gov/earthquakes/eventpage/us2000jekx\",\"detail\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us2000jekx&format=geojson\",\"felt\":2,\"cdi\":4.5,\"mmi\":4.37,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":0,\"sig\":501,\"net\":\"us\",\"code\":\"2000jekx\",\"ids\":\",us2000jekx,\",\"sources\":\",us,\",\"types\":\",dyfi,geoserve,losspager,origin,phase-data,shakemap,\",\"nst\":null,\"dmin\":1.168,\"rms\":1.1,\"gap\":91,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 5.7 - 129km NW of Kota Ternate, Indonesia\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[126.4563,1.5093,35]},\"id\":\"us2000jekx\"},{\"type\":\"Feature\",\"properties\":{\"mag\":5.5,\"place\":\"43km NE of Auki, Solomon Islands\",\"time\":1549503694180,\"updated\":1549511014814,\"tz\":660,\"url\":\"https://earthquake.usgs.gov/earthquakes/eventpage/us2000jek8\",\"detail\":\"https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us2000jek8&format=geojson\",\"felt\":3,\"cdi\":2.7,\"mmi\":4.01,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":0,\"sig\":466,\"net\":\"us\",\"code\":\"2000jek8\",\"ids\":\",us2000jek8,\",\"sources\":\",us,\",\"types\":\",dyfi,geoserve,losspager,origin,phase-data,shakemap,\",\"nst\":null,\"dmin\":1.391,\"rms\":0.85,\"gap\":50,\"magType\":\"mb\",\"type\":\"earthquake\",\"title\":\"M 5.5 - 43km NE of Auki, Solomon Islands\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[160.9592,-8.4642,65.94]},\"id\":\"us2000jek8\"}],\"bbox\":[126.4563,-8.4642,33,160.9592,2.2151,65.94]}";
-                 root = JsonConvert.DeserializeObject<RootObject>(json); 
-             }
-             finally{
-                 foreach (Feature feature in root.features)
-                 {
-                     System.Windows.Point point = ToMercator(feature.geometry.coordinates);
+        public Dictionary<System.Windows.Point, Ellipse> getQuake()
+        {
+            Dictionary<System.Windows.Point, Ellipse> points = new Dictionary<System.Windows.Point, Ellipse>();
+                
+            foreach (Feature feature in root.features)
+            {
+                System.Windows.Point point = ToMercator(feature.geometry.coordinates);
 
-                     Ellipse ellipse = new Ellipse();
-                     ellipse.ToolTip = feature.properties.place;
+                Ellipse ellipse = new Ellipse();
+                string info = "Location : " + feature.properties.place + "\n"
+                    + "Magnitude : " + feature.properties.mag + "\n"
+                    + "Felt by : " + feature.properties.felt + "\n"
+                    + "Tsunami Alert : " + feature.properties.tsunami;
 
-                     if (feature.properties.alert != null)
-                     {
-                         string color = feature.properties.alert;
-                         BrushConverter bc = new BrushConverter();
-                         Brush brush = (Brush)bc.ConvertFrom(color);
-                         ellipse.Fill = brush;
-                     }
-                     else
-                     {
-                         ellipse.Fill = Brushes.Gray;
-                     }
-                     points.Add(ellipse);
-                     Draw(point, ellipse);
-                 }
-             }
+                ellipse.ToolTip = info;
 
+                if (feature.properties.alert != null)
+                {
+                    string color = feature.properties.alert;
+                    BrushConverter bc = new BrushConverter();
+                    Brush brush = (Brush)bc.ConvertFrom(color);
+                    ellipse.Fill = brush;
+                }
+                else
+                {
+                    ellipse.Fill = Brushes.Gray;
+                }
 
+                points.Add(point, ellipse);   
+            }
+            return points;
+        }
+
+        public RootObject getData(string urll) {
+            RootObject rot = new RootObject();
+
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString(url);
+
+                 rot = JsonConvert.DeserializeObject<RootObject>(json);
+            }
+
+            return rot;
         }
 
         private System.Windows.Point ToMercator(List<double> coordinates)
@@ -99,19 +123,24 @@ namespace QuakeData
             return mercator;
         }
 
-        private void Draw(System.Windows.Point mercator, Ellipse ellipse) {
+        private void Draw(Dictionary<System.Windows.Point, Ellipse> quakeList) {
             //Ellipse ellipse = new Ellipse();
             //ellipse.Fill = Brushes.Green;
-            ellipse.Width = 10;
-            ellipse.Height = ellipse.Width;
-            ellipse.StrokeThickness = 2;
-            ellipse.MouseEnter += ellipse_MouseEnter;
-            ellipse.MouseDown += ellipse_MouseDown; 
 
-            canvas.Children.Add(ellipse);
+            foreach( System.Windows.Point mercator in quakeList.Keys){
+                Ellipse ellipse = new Ellipse();
+                ellipse = quakeList[mercator];
+                ellipse.Width = 10;
+                ellipse.Height = ellipse.Width;
+                ellipse.StrokeThickness = 2;
+                ellipse.MouseEnter += ellipse_MouseEnter;
+                ellipse.MouseDown += ellipse_MouseDown; 
 
-            Canvas.SetLeft(ellipse, mercator.X);
-            Canvas.SetTop(ellipse, mercator.Y);
+                canvas.Children.Add(ellipse);
+
+                Canvas.SetLeft(ellipse, mercator.X);
+                Canvas.SetTop(ellipse, mercator.Y);
+            }
         }
 
         private void ellipse_MouseEnter(object sender, MouseEventArgs e)
@@ -125,16 +154,22 @@ namespace QuakeData
             MessageBox.Show(root.features[0].properties.place);
         }
 
-
         private void btnUpdate_Click(object sender, RoutedEventArgs e)
         {
             DateTime start =(DateTime) dpStart.SelectedDate;
             DateTime end = (DateTime)dpEnd.SelectedDate;
-            string magnitude =(String) cbMagnitude.SelectedValue;
+            string magnitude =(String) cbMagnitude.Text;
 
-            string full_url = url; 
+            string full_url = url + "startime=" + start.ToShortDateString() 
+                + "&endtime=" + end.ToShortDateString()
+                + "&minmagnitude=" + magnitude;
+
+            lblStatus.Content = full_url;
+
+            initialiseData(full_url);
 
         }
         
+
     }
 }
